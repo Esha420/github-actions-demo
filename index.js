880b93d9fbecb7767e1cd18787241050271ad38e
Ubuntu <ubuntu@ip-172-31-33-184.ec2.internal>
2025-08-19 10:17:23 +0000

    Build and deploy

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function sum(a, b) {
-  return a + b;
-}
+const express = require('express');
+const sum = require('./sum');
 
-module.exports = sum;
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send(`Sum of 2 + 3 = ${sum(2, 3)}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
